27174ec1935f16b711c4bf788e0a3286266317af
poommin2543 <poommin2543@gmail.com>
2022-11-21 16:08:31 +0700

    rename file

---
+++ src/components/StreamVideo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div >
+    <div>
+      <input type="text" v-model="name" placeholder="NAME">
+      <input type="text" v-model="tel" placeholder="TEL">
+      <button @click="insertToContact(tel, name)">Add</button>
+    </div>
+
+    <hr>
+
+    <ul :key="key" v-for="(contact, key) in contacts">
+      <li v-if="updateKey === key">
+        <input type="text" v-model="updateName" placeholder="NAME">
+        <input type="text" v-model="updateTel" placeholder="TEL">
+        <button @click="updateContact(updateTel, updateName)">Save</button>
+      </li>
+      <li v-else>
+        {{contact.name}} : {{contact.tel}} 
+        <button @click="setUpdateContact(key, contact)">Update</button>
+        <button @click="deleteContact(key)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  // ...
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/StreamingView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Streaming />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Streaming from '@/components/StreamVideo.vue'
+
+export default {
+  name: 'StreamingView',
+  components: {
+    Streaming
+  }
+}
+</script>
