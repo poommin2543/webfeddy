f1e50314ed4517aeefb1d1e1d3b7157029e3fadc
poommin2543 <poommin2543@gmail.com>
2023-03-03 14:10:52 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,30 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import * as VueGoogleMaps from "vue2-google-maps"
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-import VueGamepad from 'vue-gamepad';
-import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon";
-import Vuetify from 'vuetify'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// createApp(App).mount('#app')
-Vue.use(Vuetify)
-Vue.component('vue-sidebar-menu-akahon', VueSidebarMenuAkahon);
-Vue.use(VueGamepad);
-Vue.use(VueMaterial)
 Vue.config.productionTip = false
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyD553x-on9CsDmxWaD3rfKg2l3MX48kov8",
-    // libraries: "places"
-    libraries: "places,drawing,visualization"
-  }
-});
+
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
