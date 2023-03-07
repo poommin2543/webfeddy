90bbbb8d2ac48bbc69cf04e67d1d23da01ca2dab
poommin2543 <poommin2543@gmail.com>
2023-03-07 11:19:17 +0700

    git update vuetify

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-janus
+# demo
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import store from './store'
+// import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify'
+Vue.use(Vuetify)
 Vue.config.productionTip = false
-
+// Vue.use(Vuetify)
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
